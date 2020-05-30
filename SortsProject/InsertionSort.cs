19c443c1afc8ef1f8775aad783a7b5762b3c452a
rvcl3n <d.rudenko.1996@gmail.com>
2020-05-30 22:35:21 +0300

    Added Insertion sort

---
+++ SortsProject/InsertionSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortsProject
+{
+    class InsertionSort : ISort
+    {
+        public void Sort(int[] arrayToSort)
+        {
+            int key, i;
+
+            for (int j = 1; j < arrayToSort.Length; j++)
+            {
+                key = arrayToSort[j];
+                i = j - 1;
+                while (i >= 0 && arrayToSort[i] > key)
+                {
+                    arrayToSort[i + 1] = arrayToSort[i];
+                    i = i - 1;
+                }
+                arrayToSort[i + 1] = key;
+            }
+        }
+    }
+}
